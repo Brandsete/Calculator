8dc21b2ad8ca33ec7708c2fe8e5f17e2f7a1cf49
Brandsete <usmabrandsamuel@gmail.com>
2024-09-09 12:43:27 -0500

    Función suma
    se define una función para sumar dos números ingresados por el usuario vía teclado

--- calculator.py
+++ calculator.py
@@ -11,3 +11,14 @@ def crear_ventana():
     vent.configure(bg=bg_color)
     def formato_resultado(r):
         return int(r) if r % 1 == 0 else r
+    def suma():
+        n1 = txt1.get()
+        n2 = txt2.get()
+        try:
+            r = float(n1) + float(n2)
+            r = formato_resultado(r)
+            txt3.delete(0, 'end')
+            txt3.insert(0, r)
+        except ValueError:
+            txt3.delete(0, 'end')
+            txt3.insert(0, "Error")
