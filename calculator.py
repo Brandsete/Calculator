9429cc583e150fc6e7e56607c1cbea2735ec6c45
Brandsete <usmabrandsamuel@gmail.com>
2024-09-09 13:22:19 -0500

    Función división
    Se define una función que divida dos números ingresados por el usuario vía teclado teniendo en cuenta excepciones donde no sea posible realizar la división, de esta manera se garantiza el correcto funcionamiento de la operación matematica

--- calculator.py
+++ calculator.py
@@ -44,3 +44,17 @@ def crear_ventana():
         except ValueError:
             txt3.delete(0, 'end')
             txt3.insert(0, "Error")
+    def division():
+        n1 = txt1.get()
+        n2 = txt2.get()
+        try:
+            r = float(n1) / float(n2)
+            r = formato_resultado(r)
+            txt3.delete(0, 'end')
+            txt3.insert(0, r)
+        except ValueError:
+            txt3.delete(0, 'end')
+            txt3.insert(0, "Error")
+        except ZeroDivisionError:
+            txt3.delete(0, 'end')
+            txt3.insert(0, "Error")
