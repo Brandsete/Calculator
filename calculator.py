07499de28ccf64aff3e22935d1234d616f959497
Brandsete <usmabrandsamuel@gmail.com>
2024-09-09 12:19:50 -0500

    Interfaz
    se definen las especificaciones para la interfaz de la ventana que verá el usuario, aquí se modifica el tamaño de la pestaña, le asignamos un un nombre y el color del fondo

--- calculator.py
+++ calculator.py
@@ -5,3 +5,7 @@ def crear_ventana():
     bg_color = "#d42bf0"
     fg_color = "#333"
     font = ("Arial", 12)
+    vent = Tk()
+    vent.title("Calculadora Simple")
+    vent.geometry("750x400")
+    vent.configure(bg=bg_color)
